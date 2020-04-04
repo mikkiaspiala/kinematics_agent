e5a48fc00775603b16ca2885263f5008b36e66fa
Mikki <miika.aspiala@gmail.com>
2020-04-04 14:05:50 +0300

    DH notation based chained forward kinematics.

--- simulator.py
+++ simulator.py
@@ -5,32 +5,10 @@ import matplotlib.pyplot as plt
 
 
 class Robot:
-    def __init__(self):
+    def __init__(self, DH_notations):
         self.links = []
-        self.angles = np.array([0, 0, 0, 0])
-        self.base = 1
-        self.shoulder = 0.12
-        self.bicep = 0.12
-        self.forearm = 0.12
+        self.update_robot_shape(DH_notations)
 
-        self.base_length = 2
-        self.shoulder_length = 1
-        self.bicep_length = 1
-        self.forearm_length = 1
-        self.scale = 1
-
-    def get_link_positions1(self):
-        link0 = np.array([0, 1, 0]) * self.scale
-
-        link1 = np.array([np.cos(self.base) * np.cos(self.shoulder) + link0[0], np.cos(self.base) * np.sin(self.shoulder) + link0[1],  np.sin(self.base) + link0[2]]) * self.scale
-
-        link2 = np.array([np.cos(self.base) * np.cos(self.bicep) + link1[0], np.cos(self.base) * np.sin(self.bicep) + link1[1], np.sin(self.base) + link1[2]]) * self.shoulder_length * self.scale
-
-        link3 = np.array([np.cos(self.base)*np.cos(self.bicep) + link2[0], np.cos(self.base)*np.sin(self.bicep) + link2[1], np.sin(self.base) + link2[2]]) * self.bicep_length * self.scale
-
-        link4 = np.array([np.cos(self.base)*np.cos(self.forearm) + link3[0], np.cos(self.base)*np.sin(self.forearm) + link3[1], np.sin(self.base) + link3[2]]) * self.forearm_length * self.scale
-
-        return np.array([[0, 0, 0], link0, link1, link2, link3, link4])
 
     def create_link(self, DH_notation):
         theta, alpha, d, r = DH_notation
@@ -40,40 +18,56 @@ class Robot:
                         [0, 0, 0, 1]])
         return A_n
 
-    def define_robot_shape(self, DH_notations):
+    def update_robot_shape(self, DH_notations):
+        self.links = []
         for DH_notation in DH_notations:
             self.links.append(self.create_link(DH_notation))
 
     def get_robot_position(self):
-        position = 1
-        for link in self.links:
-            pass
+        link1 = self.links[0]
+        link2 = np.matmul(link1, self.links[1])
+        link3 = np.matmul(link2, self.links[2])
+        link4 = np.matmul(link3, self.links[3])
+        link1 = self.extract_link_position(link1)
+        link2 = self.extract_link_position(link2)
+        link3 = self.extract_link_position(link3)
+        link4 = self.extract_link_position(link4)
+        return np.array([link1, link2, link3, link4])
 
-    def get_link_positions(self):
-        link0 = np.array([0, 1, 0])
+    def extract_link_position(self, link):
+        position = {}
+        x = link[0, 3]
+        y = link[1, 3]
+        z = link[2, 3]
+        position['xyz'] = [x, y, z]
+        x_angle = np.arctan2(link[2, 1], link[2, 2])
+        y_angle = np.arctan2(-link[2, 0], np.sqrt(link[2, 1]**2 + link[2, 2]**2))
+        z_angle = np.arctan2(link[1, 0], link[0, 0])
+        position['angle'] = [x_angle, y_angle, z_angle]
+        return position
 
-        link1 = np.array([(np.cos(self.shoulder) + link0[0])*np.cos(self.base), np.sin(self.shoulder) + link0[1], np.sin(self.base)])
-
-        link2 = np.array([(np.cos(self.bicep + self.shoulder) + link1[0])*np.cos(self.base), np.sin(self.bicep + self.shoulder) + link1[1], (np.cos(self.bicep + self.shoulder) + link1[0])*np.sin(self.base)])
-
-        link3 = np.array([(np.cos(self.bicep + self.forearm + self.shoulder) + link2[0])*np.cos(self.base), np.sin(self.forearm + self.bicep + self.shoulder) + link2[1], (np.cos(self.bicep + self.forearm + self.shoulder) + link2[0])*np.sin(self.base)])
-
-        return np.array([[0, 0, 0], link0, link1, link2, link3])
-
-    def plot_pose1(self):
-        pose = self.get_link_positions()
-        plt.plot(pose[:, 0], pose[:, 1])
+    def plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        joint_positions = self.get_robot_position()
+        stacked_joints = [joint['xyz'] for joint in joint_positions]
+        stacked_joints = np.concatenate(([[0, 0, 0]], stacked_joints))
+        print(stacked_joints)
+        ax.plot(stacked_joints[:, 0], stacked_joints[:, 1], stacked_joints[:, 2])
         plt.show()
 
-def plot_pose():
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    x = np.linspace(0, np.pi*2)
-    robot = Robot()
-    for value in x:
-        robot.base = value
-        links = robot.get_link_positions()
-        ax.plot(links[:, 0], links[:, 1], links[:, 2])
-    plt.show()
+DH_notations = [[0, -np.pi/2, 1, 0],
+                [0, 0, 0, 1],
+                [0, 0, 0, 1],
+                [0, 0, 0, 1]]
 
-plot_pose()
+robot = Robot(DH_notations)
+robot.plot()
+
+DH_notations = [[np.pi, -np.pi/2, 1, 0],
+                [0.12, 0, 0, 1],
+                [0.12, 0, 0, 1],
+                [0.12, 0, 0, 1]]
+
+robot.update_robot_shape(DH_notations)
+robot.plot()
